768491a3a476529fee0626cc0f924194f47069a1
Stefan Popov <122829719+stefffan123@users.noreply.github.com>
2024-01-14 20:40:38 +0200

    Add files via upload
    Write a program that reads a list of words from a given file (e. g. words.txt) and finds how many times each of the words occurs in another file (e. g. text.txt). Matching should be case-insensitive. The result should be written to an output text file (e. g. output.txt). Sort the words by frequency in descending order.
    NOTE: use the following structure: 
    namespace WordCount
    {
        public class WordCount
        {
            static void Main()
            {
                string wordPath = @"..\..\..\Files\words.txt";
                string textPath = @"..\..\..\Files\text.txt";
                string outputPath = @"..\..\..\Files\output.txt";
    
                CalculateWordCounts(wordPath, textPath, outputPath);
            }
    
            public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
            {
                // TODO: write your code here…
            }
        }
    }
    Examples
    words.txt	text.txt	output.txt
    quick is fault
    	-I was quick to judge him, but it wasn't his fault.
    -Is this some kind of joke?! Is it?
    -Quick, hide here…It is safer.	is - 3
    quick - 2
    fault - 1

---
+++ C# ADVANCED/9. Streams, Files and Directories/3._Word_Count/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace WordCount
+{
+    public class WordCount
+    {
+        static void Main()
+        {
+            string wordPath = @"E:\HDD D\KURS\C#\C# ADVANCED\9. Streams, Files and Directories\3._Word_Count\words.txt";
+            string textPath = @"E:\HDD D\KURS\C#\C# ADVANCED\9. Streams, Files and Directories\3._Word_Count\text.txt";
+            string outputPath = @"E:\HDD D\KURS\C#\C# ADVANCED\9. Streams, Files and Directories\3._Word_Count\output.txt";
+
+            CalculateWordCounts(wordPath, textPath, outputPath);
+        }
+
+        public static void CalculateWordCounts(string words_A, string text_B, string output_C)
+        {
+            var dictionary = new SortedDictionary<string, int>();
+            string inputWords = File.ReadAllText(words_A);
+            string[] words = inputWords.Split();
+            using var writer = new StreamWriter(output_C);
+
+            using (var reader = new StreamReader(text_B))
+            {
+                string currentSentence = reader.ReadLine();
+
+                while (currentSentence != null)
+                {
+                    foreach (var word in words)
+                    {
+                        if (currentSentence.ToLower().Contains(word))
+                        {
+
+                            if (!dictionary.ContainsKey(word))
+                            {
+                                dictionary.Add(word, 0);
+                                dictionary[word]++;
+                            }
+                            else
+                            {
+                                dictionary[word]++;
+                            }
+                        }
+                    }
+
+                    currentSentence = reader.ReadLine();
+                }
+
+                foreach (var word in dictionary.OrderByDescending(x => x.Value))
+                {
+                    writer.WriteLine($"{word.Key} - {word.Value}");
+                }
+            }
+        }
+    }
+}
+
