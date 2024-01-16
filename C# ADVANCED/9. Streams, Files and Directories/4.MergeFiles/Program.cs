d149bb178af0368381b06af6aac89f8a5aeabd30
Stefan Popov <122829719+stefffan123@users.noreply.github.com>
2024-01-16 12:07:28 +0200

    Add files via upload
    4.	Merge Files
    Write a program that reads the contents of two input text files (e. g. input1.txt and input2.txt) and merges them line by line together into a third text file (e. g. output.txt). The merging is done as follows:
    •	Line 1 from input1.txt
    •	Line 1 from input2.txt
    •	Line 2 from input1.txt
    •	Line 2 from input2.txt
    •	…
    If some of the files have more lines than the other, append at the end of the output the lines, which cannot be matched with the other file.
    NOTE: use the following structure:
    namespace MergeFiles
    {
        public class MergeFiles
        {
            static void Main()
            {
                var firstInputFilePath = @"..\..\..\Files\input1.txt";
                var secondInputFilePath = @"..\..\..\Files\input2.txt";
                var outputFilePath = @"..\..\..\Files\output.txt";
    
                MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
            }
    
            public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
            {
                // TODO: write your code here…
            }
        }
    }
    Examples
    input1.txt	input2.txt	output.txt
    1
    3
    5
    	2
    4
    6
    7	1
    2
    3
    4
    5
    6
    7

---
+++ C# ADVANCED/9. Streams, Files and Directories/4.MergeFiles/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace MergeFiles
+{
+    public class MergeFiles
+    {
+        static void Main()
+        {
+            var firstInputFilePath = @"E:\HDD D\KURS\C#\C# ADVANCED\9. Streams, Files and Directories\4.MergeFiles2\input1.txt";
+            var secondInputFilePath = @"E:\HDD D\KURS\C#\C# ADVANCED\9. Streams, Files and Directories\4.MergeFiles2\input2.txt";
+            var outputFilePath = @"E:\HDD D\KURS\C#\C# ADVANCED\9. Streams, Files and Directories\4.MergeFiles2\output.txt";
+
+            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
+        }
+
+        public static void MergeTextFiles(string first, string second, string output)
+        {
+            string[] file1 = File.ReadAllLines(first);
+            string[] file2 = File.ReadAllLines(second);
+
+            using (var writer = new StreamWriter(output))
+            {
+                int lineNumber = 0;
+                while (lineNumber < file1.Length || lineNumber < file2.Length)
+                {
+                    if (lineNumber < file1.Length)
+                    {
+                        writer.WriteLine(file1[lineNumber]);
+                    }
+
+                    if (lineNumber < file2.Length)
+                    {
+                        writer.WriteLine(file2[lineNumber]);
+                    }
+
+                    lineNumber++;
+                }
+            }
+        }
+    }
+}
