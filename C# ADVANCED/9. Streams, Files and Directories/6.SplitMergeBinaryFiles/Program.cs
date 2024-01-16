6ce1c6c4f63d06fecebc0e77522221e3c3045a16
Stefan Popov <122829719+stefffan123@users.noreply.github.com>
2024-01-16 15:32:48 +0200

    Add files via upload
    6.	Split / Merge Binary Files
    You are given an input binary file (e. g. example.png). Write a program to split it into two equal-sized files (e. g. part-1.bin and part-2.bin). When the input file size is an odd number, the first part should be 1 byte bigger than the second.
    After splitting the input file, join the obtained files into a new file (e. g. example-joined.png). The obtained result file should be the same as the initial input file.
    NOTE: use the following structure:
    namespace SplitMergeBinaryFile
    {
        public class SplitMergeBinaryFile
        {
            static void Main()
            {
                string sourceFilePath = @"..\..\..\Files\example.png";
                string joinedFilePath = @"..\..\..\Files\example-joined.png";
                string partOnePath = @"..\..\..\Files\part-1.bin";
                string partTwoPath = @"..\..\..\Files\part-2.bin";
    
                SplitBinaryFile(sourceFilePath, partOnePath, partTwoPath);
                MergeBinaryFiles(partOnePath, partTwoPath, joinedFilePath);
            }
    
            public static void SplitBinaryFile(string sourceFilePath, string partOneFilePath, string partTwoFilePath)
            {
                // TODO: write your code here…
            }
    
            public static void MergeBinaryFiles(string partOneFilePath, string partTwoFilePath, string joinedFilePath)
            {
                // TODO: write your code here…
            }
        }
    }

---
+++ C# ADVANCED/9. Streams, Files and Directories/6.SplitMergeBinaryFiles/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.IO;
+using System;
+
+namespace SplitMergeBinaryFile
+{
+    public class SplitMergeBinaryFile
+    {
+        static void Main()
+        {
+            string sourceFilePath = @"D:\D\razni\KURS\C#\C# ADVANCED\9. Streams, Files and Directories - Lab\proba\example.png";
+            string joinedFilePath = @"D:\D\razni\KURS\C#\C# ADVANCED\9. Streams, Files and Directories - Lab\proba\example-joined.png";
+            string partOnePath = @"D:\D\razni\KURS\C#\C# ADVANCED\9. Streams, Files and Directories - Lab\proba\part-1.bin";
+            string partTwoPath = @"D:\D\razni\KURS\C#\C# ADVANCED\9. Streams, Files and Directories - Lab\proba\part-2.bin";
+
+            SplitBinaryFile(sourceFilePath, partOnePath, partTwoPath);
+            MergeBinaryFiles(partOnePath, partTwoPath, joinedFilePath);
+        }
+
+        public static void SplitBinaryFile(string sourceFilePath, string partOneFilePath, string partTwoFilePath)
+        {
+            using (FileStream sourceStream = new FileStream(sourceFilePath, FileMode.Open, FileAccess.Read))
+            {
+                long totalBytes = sourceStream.Length;
+                int partSize = (int)Math.Ceiling((double)totalBytes / 2);
+
+                using (FileStream partOneStream = new FileStream(partOneFilePath, FileMode.Create, FileAccess.Write))
+                {
+                    while (partOneStream.Length < partSize)
+                    {
+                        int byteRead = sourceStream.ReadByte();
+                        if (byteRead == -1)
+                            break;
+
+                        partOneStream.WriteByte((byte)byteRead);
+                    }
+                }
+
+                using (FileStream partTwoStream = new FileStream(partTwoFilePath, FileMode.Create, FileAccess.Write))
+                {
+                    int byteRead = 0;
+                    while ((byteRead = sourceStream.ReadByte()) != -1)
+                    {
+                        partTwoStream.WriteByte((byte)byteRead);
+                    }
+                }
+            }
+        }
+
+        public static void MergeBinaryFiles(string partOneFilePath, string partTwoFilePath, string joinedFilePath)
+        {
+            using (FileStream joinedStream = new FileStream(joinedFilePath, FileMode.Create, FileAccess.Write))
+            {
+                using (FileStream partOneStream = new FileStream(partOneFilePath, FileMode.Open, FileAccess.Read))
+                {
+                    int byteRead;
+
+                    while ((byteRead = partOneStream.ReadByte()) != -1)
+                    {
+                        joinedStream.WriteByte((byte)byteRead);
+                    }
+                }
+
+                using (FileStream partTwoStream = new FileStream(partTwoFilePath, FileMode.Open, FileAccess.Read))
+                {
+                    int byteRead;
+
+                    while ((byteRead = partTwoStream.ReadByte()) != -1)
+                    {
+                        joinedStream.WriteByte((byte)byteRead);
+                    }
+                }
+            }
+        }
+    }
+}
