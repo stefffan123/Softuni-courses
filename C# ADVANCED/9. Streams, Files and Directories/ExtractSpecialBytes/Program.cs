4a6ca33cc2b0c82fea029cd7a4e9da43822ab38e
Stefan Popov <122829719+stefffan123@users.noreply.github.com>
2024-01-16 12:02:46 +0200

    Add files via upload
    5.	Extract Special Bytes
    You are given a binary file (e. g. example.png) and a text file (e. g. bytes.txt), holding a list of bytes in the range [0…255]. Write a program to extract occurrences of all given bytes from the input file to an output binary file (e. g. output.bin).
    NOTE: use the following structure:
    namespace ExtractSpecialBytes
    {
        public class ExtractSpecialBytes
        {
            static void Main()
            {
                string binaryFilePath = @"..\..\..\Files\example.png";
                string bytesFilePath = @"..\..\..\Files\bytes.txt";
                string outputPath = @"..\..\..\Files\output.bin";
    
                ExtractBytesFromBinaryFile(binaryFilePath, bytesFilePath, outputPath);
            }
    
            public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
            {
                // TODO: write your code here…
            }
        }
    }
    Examples
    example.png	bytes.txt	output.bin
     	20
    46
    183
    212

---
+++ C# ADVANCED/9. Streams, Files and Directories/ExtractSpecialBytes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ExtractSpecialBytes
+{
+    public class ExtractSpecialBytes
+    {
+        static void Main()
+        {
+            string binaryFilePath = @"D:\D\razni\KURS\C#\C# ADVANCED\9. Streams, Files and Directories - Lab\ExtractSpecialBytes\example.png";
+            string bytesFilePath = @"D:\D\razni\KURS\C#\C# ADVANCED\9. Streams, Files and Directories - Lab\ExtractSpecialBytes\bytes.txt";
+            string outputPath = @"D:\D\razni\KURS\C#\C# ADVANCED\9. Streams, Files and Directories - Lab\ExtractSpecialBytes\output.bin";
+
+            ExtractBytesFromBinaryFile(binaryFilePath, bytesFilePath, outputPath);
+        }
+
+        public static void ExtractBytesFromBinaryFile(string binaryFile, string bytesFile, string output)
+        {
+            byte[] binaryData = File.ReadAllBytes(binaryFile);
+            byte[] bytesToExtract = File.ReadAllLines(bytesFile).Select(byte.Parse).ToArray();
+
+            using (var outputStream = new FileStream(output, FileMode.Create))
+            {
+                foreach (byte b in binaryData)
+                {
+                    if (bytesToExtract.Contains(b))
+                    {
+                        outputStream.WriteByte(b);
+                    }
+                }
+            }
+        }
+    }
+}
