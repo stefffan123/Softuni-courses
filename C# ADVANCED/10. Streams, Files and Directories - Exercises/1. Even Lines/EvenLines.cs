489e704b99794443d66a14ab02c61313dd6802fa
Stefan Popov <122829719+stefffan123@users.noreply.github.com>
2024-01-17 13:20:44 +0200

    Add files via upload
    1. Even Lines
    Write a program that reads a text file (e. g. text.txt) and prints on the console its even lines. Line numbers start from 0. Use StreamReader. Before you print the result, replace {'-', ', ', '. ', '! ', '? '} with '@' and reverse the order of the words.
    Note: use the following structure:
    namespace EvenLines
    {
        using System;
    
        public class EvenLines
        {
            static void Main()
            {
                string inputFilePath = @"..\..\..\text.txt";
    
                Console.WriteLine(ProcessLines(inputFilePath));
            }
    
            public static string ProcessLines(string inputFilePath)
            {
            }
        }
    }
    
    Examples
    Input file: text.txt	Output (at the console)
    -I was quick to judge him, but it wasn't his fault.
    -Is this some kind of joke?! Is it?
    -Quick, hide here. It is safer.	fault@ his wasn't it but him@ judge to quick was @I
    safer@ is It here@ hide @Quick@

---
+++ C# ADVANCED/10. Streams, Files and Directories - Exercises/1. Even Lines/EvenLines.cs
@@ -0,0 +1,45 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Text;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"D:\D\razni\KURS\C#\C# ADVANCED\10. Streams, Files and Directories - Exercises\Skeleton-Exercise\EvenLines\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            StringBuilder result = new StringBuilder();
+
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                int lineNumber = 0;
+
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+
+                    if (lineNumber % 2 == 0)
+                    {
+                        line = line.Replace("-", "@").Replace(", ", "@").Replace(". ", "@").Replace("! ", "@").Replace("? ", "@");
+                        string[] words = line.Split(' ');
+                        Array.Reverse(words);
+                        line = string.Join(' ', words);
+
+                        result.AppendLine(line);
+                    }
+
+                    lineNumber++;
+                }
+            }        
+            return result.ToString();
+
+        }
+    }
+}
