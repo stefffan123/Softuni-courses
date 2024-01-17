45493bb8b404ce6bc6ece37d0cfcaba73b8e1f2d
Stefan Popov <122829719+stefffan123@users.noreply.github.com>
2024-01-17 13:48:04 +0200

    Add files via upload
    2. Line Numbers
    Write a program that reads a text file (e. g. text.txt) and inserts line numbers in front of each of its lines and count all the letters and punctuation marks. The result should be written to another text file (e. g. output.txt). Use the static class File to read and write all the lines of the input and output files.
    Note: use the following structure:
    public class LineNumbers
    {
        public static void ProcessLines(string inputFilePath, string outputFilePath)
        {
        }
    }
    Examples
    text.txt	output.txt
    -I was quick to judge him, but it wasn't his fault.
    -Is this some kind of joke?! Is it?
    -Quick, hide here. It is safer.	Line 1: -I was quick to judge him, but it wasn't his fault. (37)(4)
    Line 2: -Is this some kind of joke?! Is it? (24)(4)
    Line 3: -Quick, hide here. It is safer. (22)(4)

---
+++ C# ADVANCED/10. Streams, Files and Directories - Exercises/2. LineNumbers/LineNumbers.cs
@@ -0,0 +1,32 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Text.RegularExpressions;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"D:\D\razni\KURS\C#\C# ADVANCED\10. Streams, Files and Directories - Exercises\Skeleton-Exercise\LineNumbers\text.txt";
+            string outputFilePath = @"D:\D\razni\KURS\C#\C# ADVANCED\10. Streams, Files and Directories - Exercises\Skeleton-Exercise\LineNumbers\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            string[] lines = File.ReadAllLines(inputFilePath);
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    string lineWithNumber = $"Line {i + 1}: {lines[i]}";
+                    int letterCount = Regex.Matches(lines[i], @"[a-zA-Z]").Count;
+                    int punctuationCount = Regex.Matches(lines[i], @"[^\w\s]").Count;
+                    writer.WriteLine($"{lineWithNumber} ({letterCount}) ({punctuationCount})");
+                }
+            }
+        }
+    }
+}
