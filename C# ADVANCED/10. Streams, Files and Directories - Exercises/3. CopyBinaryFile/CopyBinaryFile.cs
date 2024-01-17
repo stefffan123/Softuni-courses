ccab39ffd1815d4ab21f491371ed9bf78cc1cd4d
Stefan Popov <122829719+stefffan123@users.noreply.github.com>
2024-01-17 13:15:26 +0200

    Add files via upload
    3. Copy Binary File
    Write a program that copies the contents of a binary file (e. g. copyMe.png) to another binary file (e. g. copyMe-copy.png) using FileStream. You are not allowed to use the File class or similar helper classes.
    Note: use the following structure:
    namespace CopyBinaryFile
    {
        using System;
    
        public class CopyBinaryFile
        {
            static void Main()
            {
                string inputFilePath = @"..\..\..\copyMe.png";
                string outputFilePath = @"..\..\..\copyMe-copy.png";
    
                CopyFile(inputFilePath, outputFilePath);
            }
    
            public static void CopyFile(string inputFilePath, string outputFilePath)
            {
            }
        }
    }

---
+++ C# ADVANCED/10. Streams, Files and Directories - Exercises/3. CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,30 @@
+﻿namespace CopyBinaryFile
+{
+    using System.IO;
+    using System.Reflection.PortableExecutable;
+
+    public class CopyBinaryFile
+    {
+        static void Main()
+        {
+            string inputFilePath = @"D:\D\razni\KURS\C#\C# ADVANCED\10. Streams, Files and Directories - Exercises\Skeleton-Exercise\CopyBinaryFile\copyMe.png";
+            string outputFilePath = @"D:\D\razni\KURS\C#\C# ADVANCED\10. Streams, Files and Directories - Exercises\Skeleton-Exercise\CopyBinaryFile\copyMe-copy.png";
+
+            CopyFile(inputFilePath, outputFilePath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            using (FileStream inputFileStream = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read))
+            using (FileStream outputFileStream = new FileStream(outputFilePath, FileMode.Create, FileAccess.Write))
+            {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = inputFileStream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    outputFileStream.Write(buffer, 0, bytesRead);
+                }
+            }
+        }    
+    }
+}
