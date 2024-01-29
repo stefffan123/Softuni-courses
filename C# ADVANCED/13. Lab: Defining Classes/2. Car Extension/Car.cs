969ad62a716238c2083046b487db4da6e7949262
Stefan Popov <122829719+stefffan123@users.noreply.github.com>
2024-01-29 14:36:41 +0200

    Add files via upload

---
+++ C# ADVANCED/13. Lab: Defining Classes/2. Car Extension/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+   
+    class Car
+    {
+        // Private fields
+        private string make;
+        private string model;
+        private int year;
+
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        // Public properties
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+        // Methods
+        public void Drive(double distance)
+        {
+            if (FuelQuantity - distance / 100 * FuelConsumption > 0)
+            {
+                FuelQuantity -= distance / 100 * FuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+        public string WhoAmI()
+        {
+            return ($"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}");
+        }
+    }
+}
