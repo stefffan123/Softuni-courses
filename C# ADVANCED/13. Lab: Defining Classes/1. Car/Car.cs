3d72deb08bbd1db3eb285b829874c78592437b45
Stefan Popov <122829719+stefffan123@users.noreply.github.com>
2024-01-29 14:25:36 +0200

    Add files via upload

---
+++ C# ADVANCED/13. Lab: Defining Classes/1. Car/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        // Private fields
+        private string make;
+        private string model;
+        private int year;
+
+        // Public properties
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
