e6a3ecc1550aa5ea35d78ec979f01ecff70a0483
Stefan Popov <122829719+stefffan123@users.noreply.github.com>
2024-01-30 10:02:20 +0200

    Add files via upload

---
+++ C# ADVANCED/14. Defining Classes - Exercise/1. Define a Class Person/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace DefiningClasses
+{
+    class Person
+    {
+        //------Private Fields ----------
+        private string name;
+        private int age;
+
+
+        //------ Properties -------------
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+    }
+}
---
+++ C# ADVANCED/14. Defining Classes - Exercise/1. Define a Class Person/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person
+            {
+                Name = "Peter",
+                Age = 20
+            };
+
+            Person person2 = new Person();
+            person2.Name = "George";
+            person2.Age = 18;
+
+            Person person3 = new Person();
+            person2.Name = "Jose";
+            person2.Age = 43;
+        }
+    }
+}
