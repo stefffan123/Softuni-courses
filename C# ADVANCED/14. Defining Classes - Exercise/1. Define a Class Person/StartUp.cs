5e43e7a3408b3765dfaaa0b93ecb0b93ec393ab8
Stefan Popov <122829719+stefffan123@users.noreply.github.com>
2024-01-30 21:21:18 +0200

    Update StartUp.cs

--- C# ADVANCED/14. Defining Classes - Exercise/1. Define a Class Person/StartUp.cs
+++ C# ADVANCED/14. Defining Classes - Exercise/1. Define a Class Person/StartUp.cs
@@ -1,22 +1,22 @@
-﻿namespace DefiningClasses
+namespace DefiningClasses
 {
+    // This is the entry point of your program
     public class StartUp
     {
         static void Main(string[] args)
         {
-            Person person1 = new Person
-            {
-                Name = "Peter",
-                Age = 20
-            };
+            // Creating an instance of Person using object initializer
+            Person person1 = new Person { Name = "Peter", Age = 20 };
 
+            // Creating an instance of Person and then setting properties individually
             Person person2 = new Person();
             person2.Name = "George";
             person2.Age = 18;
 
+            // Another instance of Person with properties set individually
             Person person3 = new Person();
-            person2.Name = "Jose";
-            person2.Age = 43;
+            person3.Name = "Jose";
+            person3.Age = 43;
         }
     }
 }
