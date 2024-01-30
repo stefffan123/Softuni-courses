ff68a923803f6057dec04abd47c658d0df9675b4
Stefan Popov <122829719+stefffan123@users.noreply.github.com>
2024-01-30 09:52:34 +0200

    Add files via upload
    3. Oldest Family Member
    Use your Person class from the previous tasks. Create a class Family. The class should have a list of people, a method for adding members - void AddMember(Person member) and a method returning the oldest family member – Person GetOldestMember(). Write a program that reads the names and ages of N people and adds them to the family. Then print the name and age of the oldest member. 
    Examples
    Input	Output
    3
    Peter 3
    George 4
    Annie 5	Annie 5
    5
    Steve 10
    Christopher 15
    Annie 4
    Ivan 35
    Maria 34	Ivan 35

---
+++ C# ADVANCED/14. Defining Classes - Exercise/3. Oldest Family Member/Family.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        //-------------- Constructors --------------
+        public Family()
+        {
+            this.People = new List<Person>();
+        }
+
+        //--------------- Properties ---------------
+        public List<Person> People { get; private set; }
+
+        //---------------- Methods -----------------
+        public void AddMember(Person member)
+        {
+            this.People.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return this.People.OrderByDescending(a => a.Age).FirstOrDefault();
+        }
+    }
+}
---
+++ C# ADVANCED/14. Defining Classes - Exercise/3. Oldest Family Member/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Family family = new Family();
+            int n = int.Parse(Console.ReadLine());
+            while (n-- > 0)
+            {
+                string[] personData = Console.ReadLine().Split();
+                string name = personData[0];
+                int age = int.Parse(personData[1]);
+                family.AddMember(new Person(name, age));
+            }
+
+            Console.WriteLine(family.GetOldestMember());
+        }
+    }
+}
