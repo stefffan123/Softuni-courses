1055a3598c814dcea6b3b29a088f561053ac66c8
Stefan Popov <122829719+stefffan123@users.noreply.github.com>
2024-01-31 07:43:19 +0200

    Update StartUp.cs

--- C# ADVANCED/14. Defining Classes - Exercise/3. Oldest Family Member/StartUp.cs
+++ C# ADVANCED/14. Defining Classes - Exercise/3. Oldest Family Member/StartUp.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace DefiningClasses
 {
@@ -6,8 +6,19 @@ namespace DefiningClasses
     {
         static void Main(string[] args)
         {
+            // code for a simple program that defines two classes: Person and Family.
+            // The Person class represents an individual with a name and age,
+            // the Family class represents a group of people.
+            // The program takes user input to create instances of the Person class and adds them to the Family.
+            // Finally, it prints the name and age of the oldest person in the family.
+
+            //In the Main method, an instance of the Family class is created.
             Family family = new Family();
+
+            //User input is taken to determine how many people will be added to the family(n).
             int n = int.Parse(Console.ReadLine());
+
+            //A loop reads input for each person (name and age) and adds them to the family using the AddMember method.
             while (n-- > 0)
             {
                 string[] personData = Console.ReadLine().Split();
@@ -16,6 +27,7 @@ namespace DefiningClasses
                 family.AddMember(new Person(name, age));
             }
 
+            //Finally, it prints the result of calling GetOldestMember on the Family instance.
             Console.WriteLine(family.GetOldestMember());
         }
     }
