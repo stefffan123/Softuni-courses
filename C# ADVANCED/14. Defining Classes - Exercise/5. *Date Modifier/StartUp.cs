bad4cf613fea86519dbaa212404a2175ac7c6ead
Stefan Popov <122829719+stefffan123@users.noreply.github.com>
2024-01-31 16:00:40 +0200

    Update StartUp.cs

--- C# ADVANCED/14. Defining Classes - Exercise/5. *Date Modifier/StartUp.cs
+++ C# ADVANCED/14. Defining Classes - Exercise/5. *Date Modifier/StartUp.cs
@@ -1,9 +1,13 @@
-﻿namespace P05_DateModifier
+namespace P05_DateModifier
 {
     using System;
 
     public class StartUp
     {
+        //The code is a simple C# console application
+        //that calculates the difference in days between two dates using the DateModifier class.
+        //The program takes two dates as input, converts them to DateTime objects,
+        //and then calculates the absolute difference in days.
         static void Main(string[] args)
         {
             string date1 = Console.ReadLine();
