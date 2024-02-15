b8efbaaa456fe663c61cc1543c4e6238f28b148f
Stefan Popov <122829719+stefffan123@users.noreply.github.com>
2024-02-15 07:21:48 +0200

    Add files via upload

---
+++ C# ADVANCED/17. Exercise: Generics/1. Generic Box of String/Box.cs
@@ -0,0 +1,23 @@
+﻿namespace Generics
+{
+    public class Box<T>
+    {
+        private T value;
+
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public T Value
+        {
+            get { return value; }
+            set { this.value = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"{value.GetType()}: {value}";
+        }
+    }
+}
---
+++ C# ADVANCED/17. Exercise: Generics/1. Generic Box of String/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Generics
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int numberOfEntries = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfEntries; i++)
+            {
+                var input = Console.ReadLine();
+                Box<string> box = new Box<string>(input);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
